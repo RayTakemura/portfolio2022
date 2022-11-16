d170b22c22d3006f56058c15b9674060250edf9e
unknown <raytakemura@gmail.com>
2022-11-15 21:54:56 -0800

    testing spline on web

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -22,5 +22,30 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+</style> -->
+
+<template>
+  <div class="justify-center flex bg-gray-900 items-center h-screen">
+    <div class="text-4xl text-white">
+      Hello 👋🏼
+    </div>
+    <iframe src='https://my.spline.design/untitled-e0a1d03472edbe77f3631bf8d12c4d28/' frameborder='0' width='40%' height='70%'></iframe>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
 }
 </style>
