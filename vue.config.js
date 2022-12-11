711bd091619f25caeaa55aae567198637f387954
unknown <raytakemura@gmail.com>
2022-12-11 13:40:09 -0800

    deploying

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio2022/" : "/",
+};
